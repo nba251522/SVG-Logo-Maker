8bb44a891842d2ecb978cb5217e2f8484b85903b
nba251522 <inquister112345678@gmail.com>
2023-10-24 18:25:08 -0700

    added the SVG strings for each shape in the shapes.js file

---
+++ Shapes.js
@@ -0,0 +1,5 @@
+exports.circleSVG = (color) => `<circle cx="150" cy="100" r="90" fill="${color}" />`;
+
+exports.triangleSVG = (color) => `<polygon points="150,10 280,190 20,190" fill="${color}" />`;
+
+exports.squareSVG = (color) => `<rect x="50" y="50" width="200" height="150" fill="${color}" />`;
